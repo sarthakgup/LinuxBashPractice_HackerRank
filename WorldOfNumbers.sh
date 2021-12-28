c698ccfe9019f1e392b89ce5ecf333a3da15959b
Sarthak Gupta <16374350+sarthakgup@users.noreply.github.com>
2021-12-28 13:29:45 -0800

    Create WorldOfNumbers.sh

---
+++ WorldOfNumbers.sh
@@ -0,0 +1,12 @@
+#Given integer inputs X and Y... (first line input X, 2nd is Y)
+#Find their sum, diff, product, and quotient
+
+#First reading in both lines of input into X and Y
+read X
+read Y
+
+#echoing/printing results
+echo $((X+Y))
+echo $((X-Y))
+echo $((X*Y))
+echo $((X/Y))
