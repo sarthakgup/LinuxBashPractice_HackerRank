c101cd55541cd0853cbe51df4c3127a15f5bd183
Sarthak Gupta <16374350+sarthakgup@users.noreply.github.com>
2021-12-27 20:18:34 -0800

    Create PersonalizedEcho.sh

---
+++ PersonalizedEcho.sh
@@ -0,0 +1,8 @@
+#Problem: Write a Bash script thats accept stdin input 'name' and prints "Welcome 'name'"
+# Example: 'name' = Dan, then print "Welcome Dan" 
+
+echo -n "Welcome "
+cat
+
+#-n modifier eliminates the newline after printing "Welcome "
+#cat with no modifiers (same as doing cat -) prints stdin
